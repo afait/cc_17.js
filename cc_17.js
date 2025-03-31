0ac6976f7d461bc687c9ac83af140ed21b55ade4
Annabelle Fait <imannaf8@gmail.com>
2025-03-30 23:52:21 -0400

    Task 2

--- cc_17.js
+++ cc_17.js
@@ -25,3 +25,40 @@ const customer1 = new Customer("Mickey Mouse", "ilikecheese@gmail.com");
 customer1.addPurchase(100);
 console.log(`${customer1.name} Spent: $${customer1.getTotalSpent()}`);
 
+
+// Task 2 
+
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    }
+    // Created class "SalesRep"
+
+    addClient(customer) {
+        if (customer instanceof Customer) {
+            this.clients.push(customer);
+        }
+    } // Created a method to add customers to the list
+
+    getClientTotal(name) {
+        const client = this.clients.find(customer =>customer.name === name);
+        if (client) {
+                return client.getTotalSpent();
+        }
+        return 0;
+    } // Returns the total amount the client has spent with a default of 0
+};
+
+
+// Examples
+
+const salesRep = new SalesRep("Goofy");
+salesRep.addClient(customer1);
+customer1.addPurchase(200);
+
+const customer2 = new Customer("Donald Duck", "gotanygrapes@gmail.com");
+salesRep.addClient(customer2);
+
+console.log(`Total spent by ${customer1.name}: $${salesRep.getClientTotal("Mickey Mouse")}`);
+console.log(`Total spent by ${customer2.name}: $${salesRep.getClientTotal("Donald Duck")}`);
